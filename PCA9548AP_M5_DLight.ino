8ac1cdb8a56e55d4005fda50f9dbace314c98ebf
Adam Howell <AdamJHowell@users.noreply.github.com>
2023-12-25 23:52:05 -0700

    Got all ports working.

--- PCA9548AP_M5_DLight.ino
+++ PCA9548AP_M5_DLight.ino
@@ -8,19 +8,18 @@
 #include <M5_DLight.h>
 #include "Wire.h"
 
-#define PCA_ADDRESS 0x70
 
-M5_DLight sensor0;
-M5_DLight sensor1;
-M5_DLight sensor2;
-M5_DLight sensor3;
-unsigned long loopDelay = 5000;					// The maximum value of 4,294,967,295 allows for a delay of about 49.7 days.
 unsigned long lastLoop = 0;						// Holds the time when the most recent loop completed.
 unsigned long loopCount = 0;
+const unsigned long loopDelay = 5000;					// The maximum value of 4,294,967,295 allows for a delay of about 49.7 days.
 const char * sketchName = "PCA9548AP_PortScanner";	// The name of this sketch.
-int startPort = 0x00;
-int endPort = 0x77;
-uint16_t lux;
+const int PCA_ADDRESS = 0x70;
+const int startPort = 0x00;
+const int endPort = 0x77;
+const unsigned int numSensors = 4;
+uint16_t luxArray[4];
+M5_DLight sensorArray[4];
+unsigned int sensorAddresses[4];// = {0, 1, 4, 5};
 
 
 void pcaSelect( uint8_t i )
@@ -40,24 +39,26 @@ void setup()
     delay( 1000 );
 
   Wire.begin();
-  pcaSelect( 0 );
-  sensor0.begin();
-  // CONTINUOUSLY_H_RESOLUTION_MODE
-  // CONTINUOUSLY_H_RESOLUTION_MODE2
-  // CONTINUOUSLY_L_RESOLUTION_MODE
-  // ONE_TIME_H_RESOLUTION_MODE
-  // ONE_TIME_H_RESOLUTION_MODE2
-  // ONE_TIME_L_RESOLUTION_MODE
-  sensor0.setMode( CONTINUOUSLY_H_RESOLUTION_MODE );
-  pcaSelect( 1 );
-  sensor1.begin();
-  sensor1.setMode( CONTINUOUSLY_H_RESOLUTION_MODE );
-  pcaSelect( 2 );
-  sensor2.begin();
-  sensor2.setMode( CONTINUOUSLY_H_RESOLUTION_MODE );
-  pcaSelect( 3 );
-  sensor3.begin();
-  sensor3.setMode( CONTINUOUSLY_H_RESOLUTION_MODE );
+
+  sensorAddresses[0] = 0;
+  sensorAddresses[1] = 1;
+  sensorAddresses[2] = 4;
+  sensorAddresses[3] = 5;
+  /*
+   * Sensor modes:
+   * CONTINUOUSLY_H_RESOLUTION_MODE
+   * CONTINUOUSLY_H_RESOLUTION_MODE2
+   * CONTINUOUSLY_L_RESOLUTION_MODE
+   * ONE_TIME_H_RESOLUTION_MODE
+   * ONE_TIME_H_RESOLUTION_MODE2
+   * ONE_TIME_L_RESOLUTION_MODE
+   */
+  for( uint8_t i = 0; i < numSensors; i++ )
+  {
+    pcaSelect( sensorAddresses[i] );
+    sensorArray[i].begin();
+    sensorArray[i].setMode( CONTINUOUSLY_H_RESOLUTION_MODE );
+  }
   Serial.println( "\nI2C scanner and lux sensor are ready!" );
 }
 
@@ -67,26 +68,17 @@ void loop()
 	unsigned long time = millis();
 	if( ( lastLoop == 0 ) || ( time - lastLoop ) > loopDelay )
 	{
-		Serial.printf( "\n%s will scan the I2C bus for device addresses in the range of 0x%X to 0x%X.\n", sketchName, startPort, endPort );
+		Serial.printf( "\n%s will scan the DLight sensors.\n", sketchName );
 		loopCount++;
-    // Iterate through every port on the PCA9548AP.
-    for( uint8_t port = 0; port < 8; port++ ) 
+
+    for( uint8_t i = 0; i < numSensors; i++ )
     {
-      pcaSelect( port );
-      Serial.printf( "PCA Port #%d\n", port );
-
-      lux = sensor0.getLUX();
-      Serial.printf( "Port %d lux: %d\n", port, lux );
-
-      // Iterate through every potentially valid I2C address.
-      for( uint8_t addr = 0; addr <= 127; addr++ ) 
-      {
-        if( addr == PCA_ADDRESS )
-          continue;
-        Wire.beginTransmission( addr );
-        if( !Wire.endTransmission() )
-          Serial.printf( "  Port %d has an I2C device at address 0x%X\n", port, addr );
-      }
+      pcaSelect( sensorAddresses[i] );
+      luxArray[i] = sensorArray[i].getLUX();
+    }
+    for( uint8_t i = 0; i < numSensors; i++ )
+    {
+      Serial.printf( "PCA Port # %d, lux: %d\n", sensorAddresses[i], luxArray[i] );
     }
     Serial.printf( "Completed %lu scans.  Next scan in %lu seconds.\n\n", loopCount, loopDelay / 1000 );
 		lastLoop = millis();
